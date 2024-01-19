d0a58d53fc95292471cae75d950fdbf27127a320
JO <jeremywosborne@gmail.com>
2024-01-19 09:28:45 -0800

    tweak

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,11 +5,15 @@
 // and turn off when our (quite large) game script has loaded.
 //
 document.addEventListener("DOMContentLoaded", () => {
-    const loadingContainer = document.getElementById("pk-bios-loading");
+    const gameLoadingContainer = document.getElementById("pk-bios-loading");
     const gameContainer = document.getElementById("game-container");
 
     import("./game").then((game) => {
-        loadingContainer.classList.add("loaded");
+        // The HTML page might not have a loading container.
+        if (gameLoadingContainer) {
+            gameLoadingContainer.classList.add("loaded");
+        }
+        // But it always must have a game container.
         gameContainer.classList.remove("hidden");
         game.main(); // Function to initialize and start your game
     });
